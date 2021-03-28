a5b95bf0bd8e65516d84d778981ed614ed702232
Bob Clarke <bob.m.clarke@gmail.com>
2021-03-28 23:16:58 +0100

    Trigger CI

--- terraform/modules/gke/main.tf
+++ terraform/modules/gke/main.tf
@@ -21,7 +21,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
     machine_type = "e2-medium"
 
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    service_account = google_service_account.default.email
+    service_account = var.service_account
     oauth_scopes    = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
